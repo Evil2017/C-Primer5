80793a9d9ae823f5137962d9d3d0bedc6005e90e
梁世任 <EvilArtist@outlook.com>
2019-08-22 14:54:32 +0800

    添加文件

---
+++ ch01/ex1_09.cpp
@@ -0,0 +1,17 @@
+// sum the numbers from 50 to 100 (use while)
+
+#include <iostream>
+
+int main()
+{
+    int sum = 0, val = 50;
+    while (val <= 100) {
+        sum += val;
+        ++val;
+    }
+    std::cout << "the sum is: " << sum << std::endl;
+    system("pause");
+    return 0;
+}
+
+// output: the sum is: 3825
