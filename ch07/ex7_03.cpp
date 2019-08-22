82b906bd2f6eed6208290a1c7cb13c7035bd57fa
design <276667311@qq.com>
2019-08-22 23:03:47 +0800

    测试　引用类型返回

--- ch07/ex7_03.cpp
+++ ch07/ex7_03.cpp
@@ -1,12 +1,24 @@
 #include "ex7_02_sales_data.h"
 #include <iostream>
-
-using std::cin;
-using std::cout;
-using std::endl;
-
+using namespace std;
+const std::string& shorterString(const string& s1, const string& s2)
+{
+    cout << "&s1:" << &s1 << "\t&s2:" << &s2 << endl;
+    cout << "s1:" << s1 << "\ts2:" << s2 << endl;
+    return s1.size() <= s2.size() ? s1 : s2;
+}
 int main()
 {
+    string a = "1234324";
+    string b = "abdcde";
+    string c = "";
+    cout << "&a=" << &a << "\t&b=" << &b << "\t&c=" << &c << endl;
+    cout << "a=" << a << "\tb=" << b << "\tc=" << c << endl;
+    c = shorterString(a, b);
+    cout << "&a=" << &a << "\t&b=" << &b << "\t&c=" << &c << endl;
+    cout << "a=" << a << "\tb=" << b << "\tc=" << c << endl;
+    return 0;
+
     Sales_data total;
     if (cin >> total.bookNo >> total.units_sold >> total.revenue) {
         Sales_data trans;
@@ -14,13 +26,11 @@ int main()
             if (total.isbn() == trans.isbn())
                 total.combine(trans);
             else {
-                cout << total.bookNo << " " << total.units_sold << " "
-                     << total.revenue << endl;
+                cout << total.bookNo << " " << total.units_sold << " " << total.revenue << endl;
                 total = trans;
             }
         }
-        cout << total.bookNo << " " << total.units_sold << " " << total.revenue
-             << endl;
+        cout << total.bookNo << " " << total.units_sold << " " << total.revenue << endl;
     }
     else {
         std::cerr << "No data?!" << std::endl;
