c534a2cee6b8e00efe24256fc411d1d1934ce1fe
design <276667311@qq.com>
2019-08-22 20:47:41 +0800

    Linux

---
+++ ch01/ex1_09.cpp
@@ -0,0 +1,23 @@
+// sum the numbers from 50 to 100 (use while)
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int sum = 0, val = 50;
+    while (val <= 100) {
+        sum += val;
+        ++val;
+    }
+    std::cout << "the sum is: " << sum << std::endl;
+    cout << sizeof(char) << endl;
+    //测试提交与同步
+    //再提交一次
+    //再再次提交测试
+    //第四次更改进行提交测试。
+
+    return 0;
+}
+
+// output: the sum is: 3825
