b998f189be60e8c26bcc6257a53b285a4615f43c
design <276667311@qq.com>
2019-08-22 23:02:55 +0800

    添加　方法　声明

--- ch07/ex7_02_sales_data.h
+++ ch07/ex7_02_sales_data.h
@@ -1,18 +1,19 @@
 #ifndef CP5_ex7_02_h
 #define CP5_ex7_02_h
-
 #include <string>
-
+using namespace std;
 // Add the combine and isbn members to the Sales_data
 struct Sales_data {
     std::string isbn() const { return bookNo; };
     Sales_data& combine(const Sales_data&);
-
+    double avg_private() const;
     std::string bookNo;
     unsigned units_sold = 0;
     double revenue = 0.0;
 };
-
+Sales_data add(const Sales_data&, const Sales_data&);
+ostream& print(ostream&, const Sales_data&);
+istream& read(istream&, Sales_data&);
 Sales_data& Sales_data::combine(const Sales_data& rhs)
 {
     units_sold += rhs.units_sold;
